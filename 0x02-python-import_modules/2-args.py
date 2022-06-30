25c826b5b399e11a69000cd0b8803853bb23e2b0
doscod <yeliandossa.9@gmail.com>
2022-06-30 05:53:14 -0700

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    l = len(argv)
-    print("{:d} {:s}{:s}".format(l - 1, "argument" if l <= 2 else "arguments",
-                                 "." if l == 1 else ":"))
-    for i, s in enumerate(argv):
-        if i > 0:
-            print("{:d}: {:s}".format(i, s))
+    import sys
+    argv = sys.argv[1:]
+    argv_count = len(argv)
+    i = 1
+    if argv_count is 0:
+        print("{:d} arguments.".format(argv_count))
+    elif:
+        print("{:d} argument:".format(argv_count))
+        print("{:d}: {:s}".format(i, sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(argv_count))
+        while i <= argv_count:
+            print("{:d}: {:s}".format(i, sys.argv[i]))
+            i += 1
+
+
