114d8a85552b8e150093ee5a9467a2288110e1f6
doscod <yeliandossa.9@gmail.com>
2022-07-05 05:10:15 -0700

    task 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    my_tmp_string_list = list(my_string)
+    index = 0
+    for i in my_tmp_string_list:
+        if i == 'c' or i == 'C':
+            my_tmp_string_list[index] = ""
+        index += 1
+    return "".join(my_tmp_string_list)
