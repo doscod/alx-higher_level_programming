1e2d67193b86d72daef925af6e83e3f051acc83b
doscod <yeliandossa.9@gmail.com>
2022-08-02 04:01:58 -0700

    fix issue

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-
-
+"""
+===========================
+My module with class MyList
+===========================
+"""
 class MyList(list):
+    """Class with method print_sorted"""
+    pass
+
     def print_sorted(self):
         """Method that sorted a list"""
-        print(sorted(self))
+        
+        print(sorted(list(self)))
