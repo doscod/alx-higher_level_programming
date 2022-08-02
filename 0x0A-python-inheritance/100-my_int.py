83708a766ed49552b6827247fcd33add3401e5b7
doscod <yeliandossa.9@gmail.com>
2022-08-02 05:48:31 -0700

    task 12

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,16 +1,37 @@
 #!/usr/bin/python3
 """
-=========================
-Module with MyInt class
-=========================
+=============================
+Module with a class Myint
+=============================
 """
 
 
-class MyInt (int):
-    def __ev__(self, my_int):
-        """Method that return True if self and other not equal, else false"""
-        return int(self) != my_int
+class MyInt(int):
+    """class that inherits from int"""
+    def __init__(self, my_int):
+        """for initialize a value my_int"""
+        self.my_int = my_int
 
-    def __pk__(self, my_int):
-        """Method that return True if self and other equal, else false"""
-        return int(self) == my_int
+    @property
+    def my_int(self):
+        return self.__my_int
+
+    @my_int.setter
+    def my_int(self, my_int):
+        if type(my_int) is not int:
+            raise TypeError("my_int must be an integer")
+        else:
+            self.__my_int = my_int
+
+    def __eq__(self, other):
+        """equal method"""
+        if self.my_int == other:
+            return False
+        else:
+            return True
+
+    def __ne__(self, other):
+        if self.my_int != other:
+            return False
+        else:
+            return True
