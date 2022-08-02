2e16eba4183b1b81246f1a658a8c707f26b7e4d8
doscod <yeliandossa.9@gmail.com>
2022-08-02 04:50:32 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+===================================
+Module with class BaseGeometry
+===================================
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    @classmethod
+    def area(self):
+        """Method for calculated area"""
+
+        raise Exception("area() is not implemented")
